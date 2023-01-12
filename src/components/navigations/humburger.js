0c91719862f904059d88c944a3c4a363df073757
angeluray <aeucampos@gmail.com>
2023-01-12 15:25:30 -0400

    Add burger menu display and hide

--- src/components/navigations/humburger.js
+++ src/components/navigations/humburger.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../media/logo.png';
 
@@ -8,14 +8,43 @@ function Navigation() {
     color: isActive ? 'rgb(241 245 249)' : 'rgb(51 65 85)',
   });
 
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+  //   const burger = document.getElementById('burger');
+  //   const menu = document.getElementById('menu');
+
+  //   burger.addEventListener('click', () => {
+  //     if (menu.classList.contains('hidden')) {
+  //       menu.classList.remove('hidden');
+  //     } else {
+  //       menu.classList.add('hidden');
+  //     }
+  //   })
+
   return (
     <>
       <div>
-        <nav className="flex lg:flex-col lg:max-w-xs lg:h-screen bg-slate-50 shadow-md border gap-y-24">
-          <div className="flex justify-center mt-5">
-            <img src={logo} className="w-48" alt="Hotelator. logo" />
+        <div className="px-4 cursor-pointer lg:p-2 lg:pl-5" id="burger" onClick={handleToggle} role="button" onKeyUp={handleToggle} tabIndex="-1">
+          {navbarOpen ? (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+            </svg>
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          )}
+        </div>
+        <nav className="flex lg:flex-col lg:h-screen bg-slate-50 shadow-md border gap-y-24 navigation absolute top-0 -z-50 lg:w-80" id={`${navbarOpen ? 'showMenu' : 'hide'}`}>
+          <div>
+            <div className="flex justify-center mt-5" id="menu">
+              <img src={logo} className="w-48" alt="Hotelator. logo" />
+            </div>
           </div>
-          <ul className="flex lg:flex-col lg:w-4/5 lg:self-end lg:text-lg font-sans font-extrabold">
+          <ul className="showlist flex lg:flex-col lg:w-11/12 lg:self-end lg:text-lg font-sans font-extrabold" id="menu">
             <li className="hover:bg-violet-600">
               <NavLink
                 style={myNavLinksEffect}
@@ -52,7 +81,7 @@ function Navigation() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                 </svg>
-                MY RESERVATIONS
+                RESERVATIONS
               </NavLink>
             </li>
             <li>
