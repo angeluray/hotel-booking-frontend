987e910b6efbeb8be7208b9c658e2c7594a12679
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-12 21:57:46 +0300

    Add useEffect to implement navigate

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../../redux/login/login';
@@ -25,54 +25,56 @@ const Login = () => {
 
   const { isLoggedIn, error } = useSelector((state) => state.login);
 
-  if (isLoggedIn) {
-    navigate('/', { replace: true });
-  }
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/', { replace: true });
+    }
+  }, [isLoggedIn]);
 
   if (error) {
     document.querySelector('#login-error').classList.remove('hidden');
   }
 
   return (
-    <div className="flex w-screen h-screen bg-gray-200 justify-center items-center">
+    <div className='flex w-screen h-screen bg-gray-200 justify-center items-center'>
       <form
         onSubmit={submitHandler}
-        className="flex flex-col gap-2 items-end p-9 w-sm-3/4"
+        className='flex flex-col gap-2 items-end p-9 w-sm-3/4'
       >
-        <img className="self-center w-4/12 w-sm-6/12" src="logo.png" alt="" />
-        <h2 className="w-full text-center text-3xl">Login</h2>
-        <div className="w-full">
-          <label htmlFor="email">Email</label>
+        <img className='self-center w-4/12 w-sm-6/12' src='logo.png' alt='' />
+        <h2 className='w-full text-center text-3xl'>Login</h2>
+        <div className='w-full'>
+          <label htmlFor='email'>Email</label>
           <input
-            type="email"
-            name="email"
-            placeholder="Enter your email"
+            type='email'
+            name='email'
+            placeholder='Enter your email'
             value={enteredEmail}
             onChange={emailChangeHandler}
-            autoComplete="off"
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            autoComplete='off'
+            className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
             required
           />
         </div>
-        <div className="w-full">
-          <label htmlFor="password">Password</label>
+        <div className='w-full'>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
+            type='password'
+            name='password'
+            placeholder='Enter your password'
             value={enteredPassword}
             onChange={passwordChangeHandler}
-            autoComplete="off"
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            autoComplete='off'
+            className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
             required
           />
         </div>
-        <p id="login-error" className="w-full text-red-600 hidden">
+        <p id='login-error' className='w-full text-red-600 hidden'>
           Please enter a valid name or password
         </p>
         <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 appearance-none border rounded px-3 leading-tight focus:outline-none focus:shadow-outline"
+          type='submit'
+          className='bg-blue-500 text-white py-2 appearance-none border rounded px-3 leading-tight focus:outline-none focus:shadow-outline'
         >
           Login
         </button>
