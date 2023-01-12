401731c55c41d6841de01f8886869d4b41fb84ef
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-12 11:25:03 +0300

    Modify login component

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/login/login';
 
-const Login = () => <div>Login</div>;
+const Login = () => {
+  const dispatch = useDispatch();
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const loginData = {
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+
+    dispatch(login(loginData));
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          type="email"
+          value={enteredEmail}
+          onChange={emailChangeHandler}
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          value={enteredPassword}
+          onChange={passwordChangeHandler}
+          autoComplete="off"
+        />
+        <button type="submit" className="bg-blue-500 text-white">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
