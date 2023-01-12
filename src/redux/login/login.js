9cafe9709bf4dd2d7fc19314acdaabb8bd23cbb6
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-12 10:40:30 +0300

    Add login slice

---
+++ src/redux/login/login.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+import getTokenAsync from './login-helper';
+
+const initialState = {
+  token: '',
+};
+
+const loginSlice = createSlice({
+  name: 'login',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.token = action.payload.token;
+    },
+  },
+});
+
+export const { login } = loginSlice.actions;
+
+export const getMessages = () => async (dispatch) => {
+  const token = await getTokenAsync();
+  dispatch(login(token));
+};
+
+export default loginSlice.reducer;
