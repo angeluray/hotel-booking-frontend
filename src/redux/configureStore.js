12ae95136d16cacea744883bdf5ca947919f63ee
angeluray <aeucampos@gmail.com>
2023-01-13 17:59:03 -0400

    Update configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,26 @@
+// import { combineReducers, applyMiddleware } from 'redux';
+// import { configureStore } from '@reduxjs/toolkit';
+// import thunk from 'redux-thunk';
+// import reservationReducer from './reducers/reservationsReducers';
+// import cityReducer from './reducers/citiesReducers';
+// import hotelReducer from './reducers/hotelReducers';
+// import loginReducer from './login/login';
+
 import { combineReducers, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import reservationReducer from './reducers/reservationsReducers';
-import cityReducer from './reducers/citiesReducers';
-import hotelReducer from './reducers/hotelReducers';
+import reservationsReducer from './reservations/reservationsSlice';
+import roomTypesReducer from './RoomTypes/roomTypesSlice';
+import cityReducer from './city/city';
+import hotelReducer from './hotel/hotel';
 import loginReducer from './login/login';
 
 const rootReducer = combineReducers({
-  hotels: hotelReducer,
+  hotel: hotelReducer,
   cities: cityReducer,
-  reservations: reservationReducer,
+  reservations: reservationsReducer,
   login: loginReducer,
+  type: roomTypesReducer,
 });
 
 const store = configureStore(
