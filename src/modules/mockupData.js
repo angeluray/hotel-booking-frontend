26c1e460b3fca06676425ca9ad103c2c84fe927c
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-13 12:47:12 +0300

    Fix linter errors

---
+++ src/modules/mockupData.js
@@ -0,0 +1,60 @@
+export const cities = [
+  {
+    key: 1,
+    name: 'Venezuela',
+  },
+  {
+    key: 2,
+    name: 'Addis Ababa',
+  },
+  {
+    key: 3,
+    name: 'Kigali',
+  },
+  {
+    key: 4,
+    name: 'Bahir Dar',
+  },
+  {
+    key: 5,
+    name: 'Madrid',
+  },
+];
+
+export const hotels = [
+  {
+    id: 1,
+    name: 'Hotel Hilton',
+  },
+  {
+    id: 2,
+    name: 'Hotel City',
+  },
+  {
+    id: 3,
+    name: 'Hotel Tropicana',
+  },
+  {
+    id: 4,
+    name: 'Hotel Bora',
+  },
+  {
+    id: 5,
+    name: 'Hotel Kuta',
+  },
+];
+
+export const roomTypes = [
+  {
+    id: 1,
+    name: 'Double',
+  },
+  {
+    id: 2,
+    name: 'Single',
+  },
+  {
+    id: 3,
+    name: 'King Size',
+  },
+];
