14b2aae27b4f7152570299a5e6b3712b44294d8a
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-13 16:34:46 +0300

    Fix linter errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
-import { combineReducers } from 'redux';
+import { combineReducers, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-// import thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 import reservationReducer from './reducers/reservationsReducers';
 import cityReducer from './reducers/citiesReducers';
 import hotelReducer from './reducers/hotelReducers';
@@ -17,8 +17,11 @@ const rootReducer = combineReducers({
   roomTypes: roomTypesReducer,
 });
 
-const store = configureStore({
-  rootReducer,
-});
+const store = configureStore(
+  {
+    reducer: rootReducer,
+  },
+  applyMiddleware(thunk),
+);
 
 export default store;
