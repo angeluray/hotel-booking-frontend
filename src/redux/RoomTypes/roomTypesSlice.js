dcc55a003933a7dfc81a537fcb8f07858bb092ac
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-13 13:14:25 +0300

    Add roomTypes reducer to store

---
+++ src/redux/RoomTypes/roomTypesSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const roomTypesSlice = createSlice({
+  name: 'roomTypes',
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
+
+export default roomTypesSlice.reducer;
