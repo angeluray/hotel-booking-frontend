5545c7cdb6ed5162a235bcbd7a841703f740db09
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-13 17:43:59 +0300

    fix linter error

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,16 +5,12 @@ import reservationReducer from './reducers/reservationsReducers';
 import cityReducer from './reducers/citiesReducers';
 import hotelReducer from './reducers/hotelReducers';
 import loginReducer from './login/login';
-import authReducer from './reducers/authReducers';
-import roomTypesReducer from './roomTypes/roomTypesSlice';
 
 const rootReducer = combineReducers({
   hotels: hotelReducer,
   cities: cityReducer,
   reservations: reservationReducer,
   login: loginReducer,
-  user: authReducer,
-  roomTypes: roomTypesReducer,
 });
 
 const store = configureStore(
