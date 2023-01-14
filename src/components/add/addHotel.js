3778be17193bba40cae8a38acb02e47933d9418a
angeluray <aeucampos@gmail.com>
2023-01-14 04:18:09 -0400

    Add parent component(form)

---
+++ src/components/add/addHotel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormHotel from './form';
+
+const displayForm = () => (
+  <div>
+    <section>
+      <h2>Are you a hotel owner?</h2>
+      <h3>Add a hotel</h3>
+    </section>
+    <FormHotel />
+  </div>
+);
+
+export default displayForm;
