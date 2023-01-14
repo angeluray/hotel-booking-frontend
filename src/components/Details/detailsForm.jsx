264701640874c0abbb60b1451f0d973dc85a7297
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-14 17:45:45 +0300

    refactor details form

--- src/components/Details/detailsForm.jsx
+++ src/components/Details/detailsForm.jsx
@@ -1,61 +1,123 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
 import { fetchDetails } from '../../redux/details/detailsReducer';
-import { useParams } from 'react-router-dom';
+import {
+  createReservation,
+  resetCreateReservationStatus,
+} from '../../redux/reservations/reservationsSlice';
+import { getRoomTypes } from '../../redux/roomTypes/roomTypesSlice';
+import ReservationModal from './ReservationModal';
 
-function DetailsForm() {
-
+function DetailsForm({ token }) {
+  const dispatch = useDispatch();
   let params = useParams();
   const roomId = params.roomId;
 
-  const dispatch = useDispatch();
-  const room = useSelector((state) => state.details);
-
   useEffect(() => {
-    dispatch(fetchDetails(roomId))
-  }, [dispatch]);
+    dispatch(getRoomTypes(token));
+    dispatch(fetchDetails(roomId));
+  }, []);
 
-  {
-    console.log(room);
-  }
+  let navigateTo = useNavigate();
 
-  return (
-    <>
-    <section className='flex mx-[5vw] my-[25vh] justify-between w-[80vw]'>
-      <img
-        className='w-[40vw] mr-4'
-        src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/309838581.jpg?k=7ade646d075801f8ccb110f9f92a6ea4abf2387d29b58b911a05b2c8209765f2&o=&hp=1'
-        alt='placeholder' />
-      <div className='flex flex-col items-end'>
-      <h2 className="mb-3 font-bold text-2xl">{room.name}</h2>
-        <p className='text-right mb-10'>{room.description.slice(0, 50)}</p>
-        <table>
-          <tbody className='text-right'>
-            <tr>
-            <td className='text-left'>Capacity:</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>PRICE:</td>
-              <td>${room.price}</td>
-            </tr>
-            <tr>
-            </></tbody>
-        </tr>
-        <tr>
-        <td className='text-left'>Hotel:</td>
-                <td>{room.hotel} Hotel</td>
-        </tr>
-      </tbody>
-    </table>
-      <button className='mt-auto py-3 px-8 bg-lime-400 rounded-lg'>
-        Reserve
-      </button></>
-      </div>
-    </section>
-    </>
+  const { roomDetails, loading } = useSelector((state) => state.details);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const openModal = () => {
+    setModalVisible(true);
+  };
+  const handleOk = (date, room) => {
+    const reservationData = {
+      date,
+      hotel_id: roomDetails.id,
+      room_type_id: parseInt(room),
+    };
+    dispatch(createReservation({ token, reservationData }));
+    setModalVisible(false);
+  };
+  const handleCancel = () => {
+    setModalVisible(false);
+  };
+  const { getRoomStatus, types } = useSelector((state) => state.roomTypes);
+
+  // handle alert after reservation creation
+  const { createReservationStatus } = useSelector(
+    (state) => state.reservations
   );
+  useEffect(() => {
+    if (
+      createReservationStatus === 'fulfilled' ||
+      createReservationStatus === 'rejected'
+    ) {
+      setTimeout(() => {
+        dispatch(resetCreateReservationStatus());
+      }, 3000);
+    }
+  }, [createReservationStatus]);
 
+  if (loading && !getRoomStatus === 'fulfilled')
+    return (
+      <p className='mt-[15%] ml-[35%] font-Taxicab text-2xl text-gray-600'>
+        LOADING...
+      </p>
+    );
+
+  if (roomDetails.length !== 0) {
+    return (
+      <div className='h-screen w-full bg-slate-100'>
+        <section className='flex h-3/5 flex-col items-center justify-evenly md:flex-row px-20 mt-[10rem]'>
+          {createReservationStatus === 'fulfilled' && (
+            <div className='absolute bottom-4 right-4 z-10 rounded  bg-green-200 px-4 py-2 text-green-700'>
+              Reservation succesfully created!
+            </div>
+          )}
+          {createReservationStatus === 'rejected' && (
+            <div className='absolute bottom-4 right-4 z-10 rounded bg-red-200 px-4 py-2 text-red-700'>
+              Ups! Something went wrong
+            </div>
+          )}
+
+          <div className='flex flex-col items-center md:items-end'>
+            <h2 className='mb-3 font-Taxicab text-3xl font-bold capitalize text-gray-800'>
+              {roomDetails.name.toUpperCase()}
+            </h2>
+            <p className='mb-10 md:text-right'>{roomDetails.description}</p>
+            <table>
+              <tbody className='md:text-right'>
+                <tr>
+                  <td className='py-1 px-4 text-left'>Rating:</td>
+                  <td className='py-1 px-4'></td>
+                </tr>
+                <tr className='bg-gray-200'>
+                  <td className='py-1 px-4 text-left'>Country:</td>
+                  <td className='py-1 px-4'>{roomDetails.city.name}</td>
+                </tr>
+              </tbody>
+            </table>
+            {token && (
+              <>
+                <button
+                  className='mt-12 rounded-full bg-lime-400 py-3 px-4 text-slate-50 hover:bg-lime-500'
+                  onClick={openModal}
+                >
+                  <i className='fa-solid fa-calendar-check mr-2'></i>Reserve
+                  <i className='fa-solid fa-circle-chevron-right ml-4'></i>
+                </button>
+                <ReservationModal
+                  visible={modalVisible}
+                  onOk={handleOk}
+                  onCancel={handleCancel}
+                  hotelInfo={roomDetails}
+                  roomTypes={types}
+                />
+              </>
+            )}
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default DetailsForm;
