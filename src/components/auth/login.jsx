7e8e543efb7ebf2173527b4882c80e05756d61cc
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-14 21:38:47 +0300

    Update the path for login

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { login } from '../../redux/login/login';
+import { login } from '../../redux/login/loginReducer';
 
 const Login = () => {
   const navigate = useNavigate();
