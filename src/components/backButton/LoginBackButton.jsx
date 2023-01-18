efdf47fd3ccebf992f362adb3ccfd1ba2a0a7c2e
angeluray <aeucampos@gmail.com>
2023-01-18 11:13:56 -0400

    Fix back button error and linters

---
+++ src/components/backButton/LoginBackButton.jsx
@@ -0,0 +1,16 @@
+import { ArrowBack } from '@mui/icons-material';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LoginBackButton = () => {
+  const navigate = useNavigate();
+  return (
+    <div className='absolute top-6 left-6' onClick={() => navigate(-1)}>
+      <button className='rounded-full border-4 p-2 text-gray-400 md:p-4 '>
+        <ArrowBack />
+      </button>
+    </div>
+  );
+};
+
+export default LoginBackButton;
